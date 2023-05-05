747ab6d0b64ae56c17943f60bdc1de99f9cc6bea
Jorge Prendes <jorge.prendes@gmail.com>
2023-05-05 22:05:01 +0100

    Update wirepump commit

--- cmake/wirepump.cmake
+++ cmake/wirepump.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(wirepump
   GIT_REPOSITORY https://github.com/jprendes/libwirepump.git
-  GIT_TAG        724bcd697c1d655747cf77e654186b80b2d8bbbc
+  GIT_TAG        723f579f2be401d75127b36ebf9bc46c661e5f7a
 )
 FetchContent_MakeAvailable(wirepump)
 FetchContent_GetProperties(wirepump)
