b1fd37916dc8c529ef60868e637b2efc18d86db0
Jorge Prendes <jorge.prendes@gmail.com>
2023-05-05 09:30:47 +0100

    Change cmake version to match latest Ubuntu LTS

--- cmake/wirepump.cmake
+++ cmake/wirepump.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(wirepump
   GIT_REPOSITORY https://github.com/jprendes/libwirepump.git
-  GIT_TAG        dba5eb34a975b30d0c16147884fa598a7817d3a8
+  GIT_TAG        724bcd697c1d655747cf77e654186b80b2d8bbbc
 )
 FetchContent_MakeAvailable(wirepump)
 FetchContent_GetProperties(wirepump)
